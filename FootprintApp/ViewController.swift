14d4a3be97409b97864e89e1d9a6177f8491dd2d
kambe <knb1031_ba@icloud.com>
2019-12-15 18:24:23 +0900

    Initial Commit

---
+++ FootprintApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FootprintApp
+//
+//  Created by 神戸悟 on 2019/12/14.
+//  Copyright © 2019 SatoruKambe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
