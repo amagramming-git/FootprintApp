8a76bf803373bd848c97d86f19ec5ca6d865c16b
kambe <knb1031_ba@icloud.com>
2019-12-17 14:51:20 +0900

    TableViewの基本的な画面遷移完了

---
+++ FootprintApp/ShowFootprintViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ShowFootprintViewController.swift
+//  FootprintApp
+//
+//  Created by 神戸悟 on 2019/12/17.
+//  Copyright © 2019 SatoruKambe. All rights reserved.
+//
+
+import UIKit
+
+class ShowFootprintViewController: UIViewController {
+
+    var text : String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(text)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FootprintApp/ViewController.swift
+++ FootprintApp/ViewController.swift
@@ -9,13 +9,56 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    // 7. SecondViewに渡す文字列
+    var selectedText: String?
+
+    // テーブルに表示するテキスト
+    let texts = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+    //Cellの行数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return texts.count
+    }
+    
+    //Cellのテキスト追加
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "Cell")
+
+        cell.textLabel?.text = texts[indexPath.row]
+        return cell
+    }
+    //多分セルを押した時の処理だと思う
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 8. SecondViewControllerに渡す文字列をセット
+        selectedText = texts[indexPath.row]
+
+        // 8. SecondViewControllerへ遷移するSegueを呼び出す
+        performSegue(withIdentifier: "showShowFootprintViewController", sender: nil)
+    }
+    // Segueを呼び出された時、遷移時の処理
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "showShowFootprintViewController") {
+            let secondVC: ShowFootprintViewController = segue.destination as! ShowFootprintViewController
+
+            // 11. SecondViewControllerのtextに選択した文字列を設定する
+            secondVC.text = selectedText
+        }
+    }
+    
     //AppDelegateのインスタンスを取得
     let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    @IBOutlet weak var footprintTableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        footprintTableView.delegate = self
+        footprintTableView.dataSource = self
     }
     
 }
